aadc50e8f11d72e46bf1fbe67862164004512641
zokaiG <futlouco7@gmail.com>
2018-07-21 16:37:12 -0300

    Added inputStream for both encrypt and decrypt options.

--- cripto.c
+++ cripto.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#define MAXLINE 1000
+
+//function declarations:
+char *getline(char line[],int max);
+void encrypt(char *str);
+void decrypt(char *str);
 
 int main(int argc, char **argv)
 {
+	int max = MAXLINE;
+	char line[MAXLINE];
+	char *pline;
+
 	int failedExec = 1;
 	if(strcmp(*(argv+1), "-c") == 0)
 	{	
 		failedExec = 0;
-		printf("We are about to encrypt!\n");
+		printf("Encrypting...\n");
+
+
+		while(strlen(pline = getline(line, max))>0)
+		{	
+			encrypt(pline);
+		}
+
 	}	
 	if(strcmp(*(argv+1), "-d") == 0)
 	{
 		failedExec = 0;
-		printf("We are about to decrypt!\n");
+		printf("Decrypting...\n");
+
+		
+		while(strlen(pline = getline(line, max))>0)
+		{	
+			decrypt(pline);
+		}
+
 	}
 	if(failedExec)
 	{
@@ -20,4 +44,31 @@ int main(int argc, char **argv)
 	}
 
 	return 0;
+}
+
+char *getline(char line[],int max)
+{
+	int c, i;
+
+	for (i = 0; i < max - 1 && (c=getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+
+	if (c == '\n') {
+		line[i] = c;
+		++i;
+	}
+
+	line[i] = '\0';
+
+	return line;
+}
+
+void encrypt(char *str)
+{
+	printf("line: %s",str);	
+}
+
+void decrypt(char *str)
+{
+	printf("line: %s",str);
 }
